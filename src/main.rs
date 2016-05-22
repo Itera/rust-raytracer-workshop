911d2a14193dd903b1219c5b2b1aee0db5f2d1ef
Sondre Lefsaker <sondre.lefsaker@itera.no>
2016-05-22 17:40:58 +0200

    feat: Implement Ray with tests

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate hamcrest;
 #[allow(dead_code)]
 mod vec;
 
+#[allow(dead_code)]
+mod ray;
+
 fn main() {
     println!("Hello from Rust!");
 }
---
+++ src/ray.rs
@@ -0,0 +1,42 @@
+use vec::Vec3;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray {
+            origin: origin,
+            direction: direction,
+        }
+    }
+
+    pub fn point_along_direction(&self, param: f64) -> Vec3 {
+        self.origin + self.direction * param
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use hamcrest::{ assert_that, is, equal_to };
+    use vec::Vec3;
+    use ray::Ray;
+
+    #[test]
+    fn can_init_ray() {
+        let r = Ray::new(Vec3::new(0.0, 1.0, 2.0), Vec3::new(2.0, 1.0, 0.0));
+
+        assert_that(r.origin[2], is(equal_to(2.0)));
+        assert_that(r.direction[2], is(equal_to(0.0)));
+    }
+
+    #[test]
+    fn can_find_point_along_direction() {
+        let r = Ray::new(Vec3::new(0.0, 1.0, 2.0), Vec3::new(2.0, 1.0, 0.0));
+
+        assert_that(r.point_along_direction(2.0), is(equal_to(Vec3::new(4.0, 3.0, 2.0))));
+    }
+}
