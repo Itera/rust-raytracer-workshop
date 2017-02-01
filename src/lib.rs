441a1a821ec4ac778b2aeca3c017827da28669f0
Trygve Aaberge <trygve.aaberge@itera.no>
2017-02-01 18:12:45 +0100

    style: Fix a indentation error in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -52,8 +52,8 @@ pub fn trace_scene(width: u32,
                 let v = ((height as f64 - y_trans - 1.0) + rng.next_f64()) / height as f64;
 
                 let ray = camera.create_ray(u, v);
-            color = panic!("Step 2b) Call the 'trace_ray_in_scene' function with the appropriate \
-                            parameters");
+                color = panic!("Step 2b) Call the 'trace_ray_in_scene' function with the appropriate \
+                                parameters");
             }
             color = color / num_samples as f64;
             pixels.push(color);
