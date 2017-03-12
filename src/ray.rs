5ea76a370cbcb1dfc20c439dca9135e829ab206f
Sondre Lefsaker <sondre.lefsaker@itera.no>
2017-03-13 00:03:53 +0100

    doc: update illustrations for calculations

--- src/ray.rs
+++ src/ray.rs
@@ -14,7 +14,7 @@ impl Ray {
         }
     }
 
-    pub fn point_along_direction(&self, point: f64) -> Vec3 {
+    pub fn point_along_direction(&self, delta: f64) -> Vec3 {
         panic!("Step 3a) Calculate the point along the direction of the ray. Hint: Remember to \
                 take the origin of the ray into the account of the final point")
     }
